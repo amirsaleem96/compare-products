4287d09c6238d9bd599a9d320ecc0111efe97a90
Amir Saleem <amirsaleem96@gmail.com>
2017-10-22 11:34:53 +0530

    test methods added

--- views/header/Header.jsx
+++ views/header/Header.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
+     constructor(props){
+         super(props);
+         this.state = {
+             btnText: "click me to see the magic"
+         }
+         this.showAlert = this.showAlert.bind(this);
+     }
+     showAlert(){
+         alert('Yeah! you just fired an event');
+     }
      render(){
+         var names = ['Amir','Ashish','Rajneesh','Amir'];
          return(
-             <header>Compare products easily on the go</header>
+             <header>
+                 {
+                  names.map(function(item,index){
+                      return <button key={index}>{item}</button>  
+                  })  
+                 }
+             </header>
          )
      }
 }
