2e4a3405586c8c5c214940dbc250289724933276
Amir Saleem <amirsaleem96@gmail.com>
2017-10-22 10:58:46 +0530

    footer component added

--- routes/App.jsx
+++ routes/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../views/header/Header.jsx';
+import Footer from '../views/footer/Footer.jsx';
 
 class App extends React.Component {
     render(){
         return(
             <div>
                 <Header />
+                <Footer />
             </div>
         )
     }
---
+++ views/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Footer extends React.Component {
+    render(){
+        return(
+            <footer>footer is coming from footer component</footer>
+        )
+    }
+}
+
+export default Footer;
